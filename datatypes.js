9a37e68218facce998a400ba0f4f70f409f239d2
Your Name <komalm7300@gmail.com>
2023-11-25 00:24:54 +0530

    js practice

---
+++ datatypes.js
@@ -0,0 +1,40 @@
+// data types:1. Primitive Data type 2. Nonpremitive
+//  The Primitive Data types: single value holder-- in JavaScript include Number, String, Boolean, Undefined, Null and Symbol.
+// Number
+// String
+// Boolean
+// Undefined
+// Null
+// Symbol
+//2.nonpremitive: // Object
+
+//number : whole no or decimal no
+let amount = 98.9898; //5655656
+
+//typeOf(): is used to get data type of data
+
+console.log(typeof(amount));
+
+//string dt : ''/""
+let city = 'mumbai';
+console.log(typeof(city));
+
+//Boolean: true/false
+let isFavMovie = false //"true";//string
+console.log(typeof(isFavMovie));
+
+//undefined
+let a ;
+console.log(a);
+console.log(typeof(a));
+
+let b = "undefined";
+console.log(b);
+console.log(typeof(b));
+
+//var false = "poonam" // we can not declare reserved keywords as a variable name
+
+let r = "none" //string
+
+let x="komal";
+console.log(typeof(x));
