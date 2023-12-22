b2b9be4dea3cd64ae748b47b225957dde1c7d16e
Your Name <komalm7300@gmail.com>
2023-12-22 20:05:01 +0530

    fdsggit

--- spreadOpr.js
+++ spreadOpr.js
@@ -1,26 +1,32 @@
 //sp opr : [...]:The JavaScript spread operator (...) allows us to quickly copy all or part of an existing array or object into another array or object.
 
-let  numbers = [1, 2, 3, 4, 5, 6];
+let numbers = [1, 2, 3, 4, 5, 6];
 let no2 = [...numbers]
 console.log(no2);
 
 
-let[no3,no4, ...no5] = numbers;
+let [no3, no4, ...no5] = numbers;
 console.log(no3);
 console.log(no4);
 console.log(no5);
 
-let a =[11,33,44,45];
-let b = [5,6,7,8,9];
+let a = [11, 33, 44, 45];
+let b = [5, 6, 7, 8, 9];
 let c = [...a, ...b];
 console.log(c);
 
 
-let obj = { name:"poonam", age:99, city:"pune",state:'mh'} 
-let obj1 = {...obj}
+let obj = { name: "poonam", age: 99, city: "pune", state: 'mh' }
+let obj1 = { ...obj }
 console.log(obj1);
 
-let obj2 = { name:"om", age:44, city:"pune"} 
+let obj2 = { name: "om", age: 44, city: "pune" }
 
-let obj3 = {...obj , ...obj2}
+let obj3 = { ...obj, ...obj2 }
 console.log(obj3);
+
+// practice by me
+
+let x = [1, 2, 3, 3, 4, 5, 9];
+let y = [...x];
+console.log(y);
